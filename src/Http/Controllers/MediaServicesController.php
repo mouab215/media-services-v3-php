c889b9d2b44281e946d59f5ba4c46f3cb337f858
Mourad Abouhcen <mouab215@outlook.com>
2021-03-23 10:19:25 +0100

    guzzle test

--- src/Http/Controllers/MediaServicesController.php
+++ src/Http/Controllers/MediaServicesController.php
@@ -1,15 +1,23 @@
 <?php
 
-
 namespace Mouab215\MediaServices\Http\Controllers;
 
-
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
 
 class MediaServicesController extends Controller
 {
     public function index()
     {
+        // Get Azure Ad token
+        $client = new Client();
+        $response = $client->request('POST', 'https://login.microsoftonline.com/:aadTenantDomain/oauth2/token');
+        dd($response);
+
+//        echo $response->getStatusCode(); // 200
+        echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+        echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+
         return view('media-services::test')->with('something', config('media-services.api-key' ));
     }
 }
--- src/config/media-services.php
+++ src/config/media-services.php
@@ -1,5 +1,13 @@
 <?php
 
+// Microsoft Azure MediaServices Credentials
 return [
-    'api-key' => 'SAHKFGh3uih429dawjs Jatoch Bawz'
+    'baseUri' => 'management.azure.com',
+    'clientId' => 'YOUR_CLIENT_ID_FROM_AAD_APPLICATION',
+    'clientSecret' => 'YOUR_AD_SERVICE_PRINCIPAL_KEY',
+    'tenantDomain' => 'YOUR_AZURE_AAD_TENANT_NAME',
+    'subscriptionId' => 'YOUR_SUBSCRIPTION_ID',
+    'accountName' => 'YOUR_MEDIA_ACCOUNT_NAME',
+    'storageAccountName' => 'YOUR_STORAGE_ACCOUNT_NAME',
+    'resourceGroup' => 'YOUR_RESOURCE_GROUP_NAME',
 ];
